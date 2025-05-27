38e5243e45ca0d92cb1d0777bcd68b06cf2ab7e3
amela <huseincehajicamela@gmail.com>
2025-05-27 19:43:33 +0200

    123

---
+++ script.js
@@ -0,0 +1,10 @@
+<body>
+    <h1>Promjena boje pozadine</h1>
+    <button onclick="Promjeni boju()">Promjeni boju</button>
+    <script>
+        function promijeniBoju() {
+            documnent.body.style.backrondColor="lightgreen"
+        }
+    </script>
+</body>
+
